9b95866e03b0d32fe8233e8b561324079c985418
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-02-13 11:01:04 +0100

    express done

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const serveIndex = require('serve-index');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+const app = express();
+
+app.use(express.static('..'));
+app.use(serveIndex('..', { 'icons': true }));
+
+app.use((req, res, next) => {
+  console.log('url not found', req.url);
+  next();
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Example app listening on port 3000!');
+});
