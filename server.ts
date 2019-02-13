16668ba0e542a3d64b1eaaedca424fba803759f2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-02-13 15:49:47 +0100

    access

--- server.ts
+++ server.ts
@@ -1,19 +1,34 @@
 import * as express from 'express';
 import * as serveIndex from 'serve-index';
+import * as ejs from 'ejs';
+import * as fs from 'fs';
+import * as path from 'path';
 
-import { timer } from 'rxjs';
-import { map } from 'rxjs/operators';
-
+const context = {
+  titi: 'coucou',
+  power2: x => x * x,
+};
 
 const app = express();
-const o = timer(1000).pipe(
-  map(n => 'toto')
-);
-o.subscribe(str => console.log('coucou', str));
+
+app.set('view engine', 'ejs');
+app.set('views', './tmpl');
 
 const www: string = 'www';
 app.use(express.static(www));
 app.use(serveIndex(www, { icons: true }));
+
+app.use((req, res, next) => {
+  const filename = path.resolve(__dirname, './tmpl/' + req.url + '.ejs');
+  console.log('filename', filename);
+  try {
+    fs.accessSync(filename);
+    res.render(filename, context);
+    return;
+  } catch (err) {
+    next();
+  }
+});
 
 app.use((req, res, next) => {
   console.log('url not found', req.url);
