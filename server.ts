feb0ffd37bebef1563a163b4f9445159ce983254
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-02-13 13:45:28 +0100

    ts

--- server.ts
+++ server.ts
@@ -1,17 +1,18 @@
-const express = require('express');
-const serveIndex = require('serve-index');
+import * as express from 'express';
+import * as serveIndex from 'serve-index';
 
 const app = express();
 
 const www: string = 'www';
 app.use(express.static(www));
-app.use(serveIndex(www, { 'icons': true }));
+app.use(serveIndex(www, { icons: true }));
 
 app.use((req, res, next) => {
   console.log('url not found', req.url);
   next();
-})
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
